b326253831b1d05aadcfb46bb2e878626b478f37
104620 <79134175+104620@users.noreply.github.com>
2022-12-21 04:40:49 +0100

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, VERSION } from '@angular/core';
+import { PersonComponent } from './person.component';
 
 @Component({
   selector: 'my-app',
@@ -9,12 +10,16 @@ import { Component, VERSION } from '@angular/core';
 export class AppComponent  {
   index = 1
   addNewPerson(){
-    console.log("Created"+this.index.toString)
+    console.log("Created "+this.index)
     let row = document.createElement('div');  
       row.className = 'row';
-      row.innerHTML = `
-      <br>
-      <person></person>`;
+      row.innerHTML = new PersonComponent();
+//       row.innerHTML = `
+//       <br>
+//       <p>new</p>
+//       <person></person>`;
+      row.style.width = '150px';
+      row.style.marginLeft = '10%';
       document.querySelector('.showInputField').appendChild(row);
     this.index++
   }
