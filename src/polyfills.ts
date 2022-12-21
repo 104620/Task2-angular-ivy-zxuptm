43288123e5538cfc2122a3d88cf6c6baecfc6a67
104620 <79134175+104620@users.noreply.github.com>
2022-12-21 01:07:35 +0100

    6 files modified

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,7 +1,13 @@
+import 'core-js/es6/reflect';
+import 'core-js/es7/reflect';
+import 'zone.js/dist/zone';
+import 'hammerjs';
+import 'web-animations-js';
 /**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
- *
+ 
+*
  * This file is divided into 2 sections:
  *   1. Browser polyfills. These are applied before loading ZoneJS and are sorted by browsers.
  *   2. Application imports. Files imported after ZoneJS that should be loaded before your main
